93fb8e202ed376d79ce41f9600c13960222650db
Nusab Taha <nusabtaha33@gmail.com>
2024-12-19 17:46:42 +0600

    lots of works are left to do

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,8 +5,8 @@ import CoolButton from "./ui/CoolButton";
 
 const Hero = ({ lang }: { lang: string }) => {
   return (
-    <div className="my-2 flex flex-col items-center justify-center gap-2 px-3 text-start md:my-4 md:flex-row md:justify-between">
-      <div className="flex items-center justify-start md:gap-3 lg:gap-5">
+    <div className="my-2 flex flex-col items-center justify-center gap-2 md:gap-0 px-3 text-start md:my-4 md:flex-row md:justify-between">
+      <div className="flex items-center justify-start md:gap-1 lg:gap-5">
         <CoolButton className="">
           শহীদদের সংখ্যা <span className="text-red-600">১২০০+</span>
         </CoolButton>
@@ -16,12 +16,13 @@ const Hero = ({ lang }: { lang: string }) => {
             {lang == "en"
               ? totalMartyrs
               : DateConverter.toBengali(String(totalMartyrs))}
-          </span>
+          </span>{" "}
+          জন
         </CoolButton>
       </div>
       <Link
         href="https://forms.gle/efEVqZEHHR4fZuyG7"
-        className="rounded-full bg-red-600 hover:bg-red-700 transition-colors duration-100 p-3 text-lg font-bold text-gray-100 md:p-4 md:text-2xl lg:text-3xl"
+        className="rounded-full bg-red-600 p-3 text-lg font-bold text-gray-100 transition-colors duration-100 hover:bg-red-700 md:p-4 md:text-2xl lg:text-3xl"
         target="_blank"
       >
         শহীদদের তথ্য দিন
