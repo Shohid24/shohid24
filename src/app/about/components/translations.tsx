ed879765ae77f07dbacc047d935db26f636c261f
Nusab Taha <nusabtaha33@gmail.com>
2024-12-25 07:26:03 +0600

    add language codes to translations for English and Bangla

--- src/app/about/components/translations.tsx
+++ src/app/about/components/translations.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
 export const ENGLISH = {
+  lang: "en",
   title: "About Shohid24 - Listing all the martyrs of July Movement",
   giveUsInfo: (
     <>
@@ -60,6 +61,7 @@ export const ENGLISH = {
 };
 
 export const BANGLA = {
+  lang: "bn",
   logoName: "শহীদ২৪",
   title:
     "শহীদ২৪ ওয়েবসাইটটি বাংলাদেশের জুলাই ছাত্র আন্দোলনে শহীদদের একটি তালিকা করেছে। এর মাধ্যমে স্বৈরাচারী শেখ হাসিনা কীভাবে হাজার-হাজার নিস্পাপ প্রাণ কেড়ে নিয়েছে সেটার একটি ধারণা পাওয়া যায়।",
