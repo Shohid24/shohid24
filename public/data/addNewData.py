297bd3019ec6ded7644e00cc191c555527334a9e
Nusab Taha <nusabtaha33@gmail.com>
2024-12-30 12:48:13 +0600

    .

---
+++ public/data/addNewData.py
@@ -0,0 +1,39 @@
+import os, json
+
+from getID import getNthCombination
+
+
+
+# add the new datas
+files = os.listdir("new")
+currentLength = len(os.listdir("profiles"))
+
+index = -1
+for index, file in enumerate(files):
+    with open(f"new/{file}", "rb") as f:
+        data = json.load(f)
+
+    id = getNthCombination(currentLength + index)
+    age = data["age"]
+    dob = data["dob"]
+    bn = data["bn"]
+    en = data["en"]
+    newData = {
+        "id": id,
+        "age": age,
+        "dob": dob,
+        "bn": bn,
+        "en": en,
+    }
+    with open(f"profiles/{id}.json", "w", encoding="utf8") as f:
+        json.dump(newData, f, ensure_ascii=False)
+    
+    try:
+        os.remove(f"new/{file}")
+        print(f"Removed new/{file}")
+    except Exception as e:
+        print(f"Failed to remove new/{file}: {e}")
+
+
+print(f"Current Profiles:", currentLength + index + 1)
+
--- public/data/main.py
+++ public/data/main.py
@@ -1,5 +1,4 @@
 import os, json
-from getID import getNthCombination
 
 with open("searchableData.json", "rb") as f:
     data = json.load(f)
@@ -59,38 +58,4 @@ for i in range(len(rawData)):
         json.dump(newData, f, ensure_ascii=False)
 
     print(f"{i:03}. {id}")
-
-
-# add the new datas
-files = os.listdir("new")
-currentLength = len(os.listdir("profiles"))
-
-index = -1
-for index, file in enumerate(files):
-    with open(f"new/{file}", "rb") as f:
-        data = json.load(f)
-
-    id = getNthCombination(currentLength + index)
-    age = data["age"]
-    dob = data["dob"]
-    bn = data["bn"]
-    en = data["en"]
-    newData = {
-        "id": id,
-        "age": age,
-        "dob": dob,
-        "bn": bn,
-        "en": en,
-    }
-    with open(f"profiles/{id}.json", "w", encoding="utf8") as f:
-        json.dump(newData, f, ensure_ascii=False)
-    
-    try:
-        os.remove(f"new/{file}")
-        print(f"Removed new/{file}")
-    except Exception as e:
-        print(f"Failed to remove new/{file}: {e}")
-
-
-print(f"Current Profiles:", currentLength + index + 1)
 
